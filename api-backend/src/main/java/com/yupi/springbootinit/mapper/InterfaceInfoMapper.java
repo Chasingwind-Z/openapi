5de7262c1ab420a18616f45477d79bc2d6eb00fc
classDog <1367488808@qq.com>
2023-03-16 08:02:29 +0800

    创建项目

---
+++ api-backend/src/main/java/com/yupi/springbootinit/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+
+/**
+* @author admin
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-03-15 17:24:19
+* @Entity com.yupi.springbootinit.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
